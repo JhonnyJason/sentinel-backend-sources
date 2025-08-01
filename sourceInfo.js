953b4f592ddea713db81f962e62e135ab89f7778
Lenard Frommelt <lenny09918050@gmail.com>
2025-08-01 21:46:09 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,7 +1,8 @@
 module.exports = {
     getDependencies: () => {
         return {
-    	    "thingy-sci-ws-base":"^0.0.1"
+    	    "thingy-sci-ws-base":"^0.0.1",
+            "xlsx": "^0.18.5"
         }
     }
 }
