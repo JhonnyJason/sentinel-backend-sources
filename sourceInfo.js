b726a38686c3915e68497cccebdd274540235fcd
Lenard Frommelt <lenny09918050@gmail.com>
2025-11-22 11:53:27 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -2,7 +2,7 @@ module.exports = {
     getDependencies: () => {
         return {
             "cached-persistentstate": "^0.0.1",
-    	    "thingy-sci-ws-base":"^0.0.1",
+            "ws": "^8.18.3",
             "xlsx": "^0.18.5",
             "yauzl": "^3.2.0"
         }
